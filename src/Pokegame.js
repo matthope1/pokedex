18814f070c1474e8f8e29ba847f4f8cc2d5bb1c0
Matthew <matt-hope@hotmail.com>
2020-10-27 12:45:02 -0400

    created get random numbers function

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -6,15 +6,48 @@ import Pokedex from './Pokedex';
 // them into two hands of 4 cards each
 // it should then render two pokedex compoenents one for each hand
 
+function getRandomNumbers(n) {
+    const randomNumList= [];
+
+    let randomNum;
+
+    while (randomNumList.length < 4){ 
+        randomNum = Math.floor(Math.random() * 8);
+        if (!randomNumList.includes(randomNum)) {
+            randomNumList.push(randomNum);
+        }
+    }
+
+    return randomValues;
+}
+
+
+function populateHands(data) {
+
+    const randomIndexes = getRandomNumbers(4);
+
+}
+
 class Pokegame extends Component {
 
     render() {
         const data = this.props.data;
+        // pick 4 random indexes from data
 
-        console.log(data);
+        populateHands(data);
+
+        const handOne = [];
+        const handTwo = [];
+
         return(
             <div className="Pokegame">
+                <div className="hand">
 
+                </div>
+
+                <div className="hand">
+
+                </div>
             </div>
         )
     }
